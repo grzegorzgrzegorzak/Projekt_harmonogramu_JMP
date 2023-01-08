ba268627b248149eef3fc8d62f81ca8c08f0b0f8
grzegorzgrzegorzak <grzegorz.grzegorzak94@gmail.com>
2023-01-08 23:04:27 +0100

    Checking style css. Ctrl+Shift+R Hard refreshing

--- Harmonogram/Harmonogram/settings.py
+++ Harmonogram/Harmonogram/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 # import os
 # import django
 # # os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
@@ -122,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
