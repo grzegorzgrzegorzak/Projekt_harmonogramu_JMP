d36c9d76b3de8b248fbc497dccee7c50f1466ea3
grzegorzgrzegorzak <grzegorz.grzegorzak94@gmail.com>
2023-01-10 22:09:17 +0100

    creating table

--- Harmonogram/schedule/admin.py
+++ Harmonogram/schedule/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-# from .models import *
+from .models import *
 
-# admin.site.register(StoreType)
-# admin.site.register(Supervisor)
-# admin.site.register(Region)
-# admin.site.register(Subcontractor)
-# admin.site.register(Store)
-# admin.site.register(MaterialConfiguration)
+admin.site.register(StoreType)
+admin.site.register(Supervisor)
+admin.site.register(Region)
+admin.site.register(Subcontractor)
+admin.site.register(Store)
+admin.site.register(MaterialConfiguration)
