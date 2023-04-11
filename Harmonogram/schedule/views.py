d14eca946e38e2a37e751d5d60ebcb39e5944bab
grzegorzgrzegorzak <grzegorz.grzegorzak94@gmail.com>
2023-04-11 22:14:37 +0200

    Dynamic URLS - Changed

--- Harmonogram/schedule/views.py
+++ Harmonogram/schedule/views.py
@@ -21,16 +21,24 @@ def schedule(request):
     inrealization = Store.objects.filter(
         date_start_installation__lte=datetime.now().date(),
         date_opening__gt=datetime.now().date()).count()
-    context = {'stores': stores, 'in30days': in30days, 'inrealization': inrealization}
+    context = {'stores': stores, 'in30days': in30days,
+               'inrealization': inrealization}
     return render(request, 'schedule/schedule.html', context)
 
 
-def generate(request, pk):
-    if not pk:
-        return redirect(reverse('generate'))
-    try:
-        store = Store.objects.get(id=pk)
-    except Store.DoesNotExist:
-        return render(request, '404.html', status=404)
-    context = {'store': store}
-    return render(request, 'schedule/generate.html', context)
+def generate(request, pk=None):
+    store = Store.objects.get(id=pk)
+    contex = {'store': store}
+    return render(request, 'schedule/generate.html', contex)
+
+# def generate(request, pk):
+#
+#     if not pk:
+#         return redirect(reverse('generate'))
+#     try:
+#         store = Store.objects.get(id=pk)
+#     except Store.DoesNotExist:
+#         context = {'store': store, 'pk': pk}
+#         return render(request, '404.html', context, status=404)
+#     context = {'store': store, 'pk': pk}
+#     return render(request, 'schedule/generate.html', context)
