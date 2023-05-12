d42409e7923f520b9d5074b5d2d4d6a31834491b
grzegorzgrzegorzak <grzegorz.grzegorzak94@gmail.com>
2023-05-12 22:20:36 +0200

    Rows colors - not AJAX, only choosing which class is used to describe row

--- Harmonogram/static/js/script.js
+++ Harmonogram/static/js/script.js
@@ -3,20 +3,22 @@ const helloWorldBox = document.getElementById('rowsd')
 const storeBox = document.getElementById('rowsd')
 
 
-$(function() {
-    $.get('/region_color_row/', function(response) {
-        var store = response.data;
-        var $element = $('.color-row');
-        store.forEach(el => {
-            if (el.region === 'LUB')    {
-                console.log(el);
-                console.log(store);
-                $element.css('background-color', 'green');
-
-            } else {
-                console.log(el.region)
-                $element.css('background-color', 'red');
-            }
-        });
-    });
-});
+//$(function() {
+//    $.get('/region_color_row/', function(response) {
+//        var store = response.data;
+//        var $element = $('.color-row');
+//        store.forEach(el => {
+//            if (el.region === 'LUB')    {
+//                console.log(el);
+//                console.log(store.indexOf(el));
+//                console.log(store);
+//                $element.css('background-color', 'green');
+//                console.log(store.splice(store.indexOf(el), 1))
+//
+//            } else {
+//                console.log(el.region)
+//                $element.css('background-color', 'red');
+//            }
+//        });
+//    });
+//});
